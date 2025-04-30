a03dd311bae1b796a63de3976d58282816c0bcba
ratnaphp <ra7709702@gmail.com>
2025-04-30 12:11:11 +0600

    Operators and Expressions

--- operators.php
+++ operators.php
@@ -2,7 +2,7 @@
 
 //Operators and Expressions
 
-echo <h1>Arithmetic Operators</h1>
+echo "<h1>Arithmetic Operators</h1>";
 /*$a = 10;
 $b = 3;
 echo $a + $b;   // 13
@@ -20,7 +20,7 @@ echo $r / $f;  //2
 echo $r % $f;  //2
 
 
-echo <h1>Assignment</h1>
+echo "<h1>Assignment</h1>";
 /*
 $x = 5;
 $x += 3;   // $x = $x + 3; $x = 8
@@ -31,7 +31,7 @@ $y += 2;
 $y *= 3;
 
 
-echo <h1>Comparison</h1>
+echo "<h1>Comparison</h1>";
 $a = 5;
 $b = "5";
 
@@ -40,11 +40,7 @@ var_dump($a === $b);  // false (value and type)
 var_dump($a != $b);   // $r == $ -> ! false
 var_dump($a !== $b);  // $r === $f -> ! true
 
-echo <h1>Logical</h1>
+echo "<h1>Logical</h1>";
 if ($a > 2 && $b < 10) { echo "Valid"; }
 if ($a > 5 || $b < 2) { echo "One is true"; }
-
-
-
-
 
