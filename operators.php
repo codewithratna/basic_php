bc46bf3c0a2f582b7af5ee30765f8d11b828a72e
ratnaphp <ra7709702@gmail.com>
2025-04-30 12:02:32 +0600

    Operators and Expressions

---
+++ operators.php
@@ -0,0 +1,50 @@
+<?php 
+
+//Operators and Expressions
+
+echo <h1>Arithmetic Operators</h1>
+/*$a = 10;
+$b = 3;
+echo $a + $b;   // 13
+echo $a - $b;   // 7
+echo $a * $b;   // 30
+echo $a / $b;   // 3.33
+echo $a % $b;   // 1*/
+
+$r = 20;
+$f = 10;
+echo $r + $f;   //30
+echo $r - $f;  //10
+echo $r * $f;   //200
+echo $r / $f;  //2
+echo $r % $f;  //2
+
+
+echo <h1>Assignment</h1>
+/*
+$x = 5;
+$x += 3;   // $x = $x + 3; $x = 8
+$x *= 2;   // $x = $x * 2; $x = 16*/
+
+$y = 10;
+$y += 2; 
+$y *= 3;
+
+
+echo <h1>Comparison</h1>
+$a = 5;
+$b = "5";
+
+var_dump($a == $b);   // true (value)
+var_dump($a === $b);  // false (value and type)
+var_dump($a != $b);   // $r == $ -> ! false
+var_dump($a !== $b);  // $r === $f -> ! true
+
+echo <h1>Logical</h1>
+if ($a > 2 && $b < 10) { echo "Valid"; }
+if ($a > 5 || $b < 2) { echo "One is true"; }
+
+
+
+
+
